60ee98565d47b472c0c8f8bdc9f9eae3029fdb58
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-29 17:37:39 +0100

    importing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header"
+import movies from "./data/data"
+import TopOne from './Components/TopOne';
+import TopThree from './Components/TopThree'
+import AllMovies from './Components/AllMovies';
 
 function App() {
+  const topOneMovie = movies.sort((a, b) => b.rating - a.rating)[0];
+  const topThreeMovies = movies.sort((a,b) => b.rating - a.rating).slice(1, 4)
+  // const movies = movies.map
   return (
     <div className="App">
       <Header />
+      <TopOne movie={topOneMovie}/>
+      <TopThree movies={topThreeMovies}/>
+      <AllMovies/>
     </div>
   );
 }
