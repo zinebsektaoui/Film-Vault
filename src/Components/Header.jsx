18f3a3653b0b662dd9523a9f2cb367f6fe3dd2d4
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-29 10:13:17 +0100

    Création des components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <header className="navbar">
+            <div className="logo">
+                <i className="fa-solid fa-building-columns" style={{color: "#0047ec"}}></i> Film<span>Vault</span>
+            </div>
+        </header>
+    )
+}
