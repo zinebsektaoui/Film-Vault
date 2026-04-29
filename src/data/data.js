21c6fc36f0582c330c27ff87150f40a1cc031153
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-29 11:41:05 +0100

    liste des films (data)

---
+++ src/data/data.js
@@ -0,0 +1,115 @@
+const movies = [
+  {
+    id: 1,
+    title: "The Flu",
+    description:
+      "Chaos spreads after a deadly airborne virus infects an entire city.",
+    year: 2013,
+    genre: "Thriller",
+    director: "Kim Sung-su",
+    actors: ["Jang Hyuk", "Soo Ae", "Park Min-ha"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMTQ0NjYzODAxMF5BMl5BanBnXkFtZTgwNTM0NjQxMDE@._V1_.jpg",
+    rating: 5
+  },
+
+  {
+    id: 2,
+    title: "Straw",
+    description:
+      "A psychological crime thriller full of mystery and suspense.",
+    year: 2020,
+    genre: "Crime",
+    director: "Tyler Perry",
+    actors: ["Taraji P. Henson", "Sherri Shepherd"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzY5Y2Q3OTItYjY0Yi00Y2MxLTg3NzUtNzYzYjM5ZjVkYTI3XkEyXkFqcGc@._V1_.jpg",
+    rating: 5
+  },
+
+  {
+    id: 3,
+    title: "The Pianist",
+    description:
+      "The story of a brilliant pianist surviving World War II.",
+    year: 2002,
+    genre: "Drama",
+    director: "Roman Polanski",
+    actors: ["Adrien Brody", "Thomas Kretschmann"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzBjM2I1YjEtNzNhNy00N2Y1LWE4OTgtNzU0M2I2ZjQ4ODQ0XkEyXkFqcGc@._V1_.jpg",
+    rating: 5
+  },
+
+  {
+    id: 4,
+    title: "The Silence",
+    description:
+      "A family struggles to survive mysterious creatures hunting by sound.",
+    year: 2019,
+    genre: "Horror",
+    director: "John R. Leonetti",
+    actors: ["Stanley Tucci", "Kiernan Shipka"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZjhkZjM2N2MtNmRhZi00M2M2LWE5NzktNzRiY2QzYzIxOGE4XkEyXkFqcGc@._V1_.jpg",
+    rating: 4.5
+  },
+
+  {
+    id: 5,
+    title: "Bird Box",
+    description:
+      "Survivors must avoid looking at terrifying supernatural entities.",
+    year: 2018,
+    genre: "Horror",
+    director: "Susanne Bier",
+    actors: ["Sandra Bullock", "Trevante Rhodes"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMjAxNjY3NjQ1OF5BMl5BanBnXkFtZTgwNjQ3ODAyNjM@._V1_.jpg",
+    rating: 4.8
+  },
+
+  {
+    id: 6,
+    title: "My Name",
+    description:
+      "A woman joins a criminal organization to avenge her father.",
+    year: 2021,
+    genre: "Action",
+    director: "Kim Jin-min",
+    actors: ["Han So-hee", "Park Hee-soon"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZTRjN2ExYjEtMjU2My00NzM0LTkwZjMtMGE2ZjliNTYxMmQ0XkEyXkFqcGc@._V1_.jpg",
+    rating: 4.9
+  },
+
+  {
+    id: 7,
+    title: "Tokyo Godfathers",
+    description:
+      "Three homeless people discover an abandoned baby on Christmas Eve.",
+    year: 2003,
+    genre: "Animation",
+    director: "Satoshi Kon",
+    actors: ["Toru Emori", "Yoshiaki Umegaki"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjY4MF5BMl5BanBnXkFtZTcwNjM0MDYyMQ@@._V1_.jpg",
+    rating: 4
+  },
+
+  {
+    id: 8,
+    title: "Parasite",
+    description:
+      "A poor family infiltrates the life of a wealthy household.",
+    year: 2019,
+    genre: "Thriller",
+    director: "Bong Joon-ho",
+    actors: ["Song Kang-ho", "Cho Yeo-jeong"],
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MGUtMzQ0Zi00YzQ0LWExNzYtYzQ1NTU3N2Y5YmQ0XkEyXkFqcGc@._V1_.jpg",
+    rating: 4.8
+  }
+];
+
+export default movies;
