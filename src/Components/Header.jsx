d2fdb58c47a176b2530be6fc6fb632046f69dd9e
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-29 11:24:26 +0100

    Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,30 @@
-export default function Navbar() {
-    return (
-        <header className="navbar">
-            <div className="logo">
-                <i className="fa-solid fa-building-columns" style={{color: "#0047ec"}}></i> Film<span>Vault</span>
-            </div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="header">
+      {/* Logo */}
+      <div className="logo">
+        <span className="logo-icon">▶</span>
+        <h1>
+          FILM<span>VAULT</span>
+        </h1>
+      </div>
+
+      {/* Actions */}
+      <div className="header-actions">
+        {/* Filter */}
+        <select className="genre-select">
+          <option value="">Genre</option>
+          <option value="Action">Action</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Drama">Drama</option>
+          <option value="Horror">Horror</option>
+          <option value="Animation">Animation</option>
+          <option value="Crime">Crime</option>
+        </select>
+
+        {/* Button */}
+        <button className="add-btn">+ Ajouter un film</button>
+      </div>
+    </header>
+  );
 }
