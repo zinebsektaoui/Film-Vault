2f661933abce38835db7b6ed3020c808c938f237
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-30 16:41:49 +0100

    add pop-up component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import Header from "./Components/Header"
-import movies from "./data/data"
+import moviesData from "./data/data"
 import TopOne from './Components/TopOne';
 import TopThree from './Components/TopThree'
 import AllMovies from './Components/AllMovies';
+import AddForm from './Components/AddForm';
+import { useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState(moviesData)
   const topOneMovie = movies.sort((a, b) => b.rating - a.rating)[0];
   const topThreeMovies = movies.sort((a,b) => b.rating - a.rating).slice(1, 4)
-  // const movies = movies.map
   return (
     <div className="App">
       <Header />
       <TopOne movie={topOneMovie}/>
       <TopThree movies={topThreeMovies}/>
       <AllMovies/>
+      <AddForm setMovies={setMovies}/>
     </div>
   );
 }
