3c6eb873f0df84523c7901a01145f44cf610c9a6
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-05-02 22:29:16 +0200

    l'implementation de la logique de detail

--- src/Components/AllMovies.jsx
+++ src/Components/AllMovies.jsx
@@ -1,13 +1,35 @@
 import movies from "../data/data";
+import { useState } from "react";
+import GetDetails from "./GetDetails";
 
-export default function AllMovies({ movies }) {  
+export default function AllMovies({ movies, onDelete }) {
+  const [selectedMovie, setSelectedMovie] = useState(null);// tp stock the selected moviee
+
+  const [menuOpen, setMenuOpen] = useState(null); // Stocke l'ID du film dont le menu est ouvert
+   const handleMenuToggle = (movieId, e) => {
+    e.stopPropagation(); // Empêche la propagation du clic
+    setMenuOpen(menuOpen === movieId ? null : movieId);
+  };
+  const handleClickOutside = () => {
+    setMenuOpen(null);
+  };
   return (
-    <section className="all-movies">
-      <h2 className="all-movies-title">TOUS LES FILMS</h2>
-
+    <section className="all-movies" onClick={handleClickOutside}>
+      <div className="top-up">
+        <h2 className="all-movies-title">TOUS LES FILMS</h2>
+        <select className="genre-select">
+          <option value="">Genre</option>
+          <option value="Action">Action</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Drama">Drama</option>
+          <option value="Horror">Horror</option>
+          <option value="Animation">Animation</option>
+          <option value="Crime">Crime</option>
+        </select>
+      </div>
       <div className="all-movies-container">
         {movies.map((movie) => (
-          <div className="movieCard" key={movie.id}>  {/* ← utilise id au lieu de title */}
+          <div className="movieCard" key={movie.id}>
             <img className="movie-image" src={movie.image} alt={movie.title} />
 
             <div className="movieContent">
@@ -21,11 +43,27 @@ export default function AllMovies({ movies }) {
               <p className="movie-year">{movie.year}</p>
               <p className="movie-genre">{movie.genre}</p>
 
-              <button className="movie-options">⋮</button>
+              <div className="movie-options-container">
+                <button className="movie-options" onClick={(e) => handleMenuToggle(movie.id, e)}>⋮</button>
+                {/* Mini Card Menu */}
+                {menuOpen === movie.id && (
+                  <div className="options-menu">
+                    <button className="menu-item details" onClick={() => setSelectedMovie(movie)}>
+                      <span>ℹ️</span> Détails
+                    </button>
+                    <button className="menu-item delete" onClick={() => onDelete(movie.id)}>
+                      <span>🗑️</span> Supprimer
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         ))}
       </div>
+      {selectedMovie &&
+        <GetDetails movie={selectedMovie} onClose={() => setSelectedMovie(null)} onDelete={onDelete} />
+      }
     </section>
   );
 }
